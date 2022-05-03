3454e9d48fd6d3616d12a1a97d21c8420192925b
j a <bj.texasaldens@gmail.com>
2022-05-03 17:33:21 -0500

    added basic Match submission and storage functionality

---
+++ ScoutApp/src/main/java/com/jacn/scoutapp/Robot.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jacn.scoutapp;
+
+/**
+ *
+ * @author bjtex
+ */
+public class Robot {
+    private int score;
+    
+    private int matchNumber;
+    
+    private int fieldPlacement;
+    
+    private int teamNumber;
+    
+    private String allianceColor;
+    
+    private String scoutName;
+    
+    private String comments;
+    
+    private Auto auto;
+    private TeleOp teleop;
+    private EndGame endgame;
+
+    public Robot(int matchNumber, String allianceColor, int fieldPlacement, int teamNumber, String scoutName, String comments, Auto auto, TeleOp teleop, EndGame endgame) {
+        this.matchNumber = matchNumber;
+        this.fieldPlacement = fieldPlacement;
+        this.teamNumber = teamNumber;
+        this.scoutName = scoutName;
+        this.comments = comments;
+        this.auto = auto;
+        this.teleop = teleop;
+        this.endgame = endgame;
+        this.allianceColor = allianceColor;
+        
+        this.score = auto.getScore() + teleop.getScore() + endgame.getScore();
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getMatchNumber() {
+        return matchNumber;
+    }
+
+    public int getFieldPlacement() {
+        return fieldPlacement;
+    }
+
+    public int getTeamNumber() {
+        return teamNumber;
+    }
+
+    public String getScoutName() {
+        return scoutName;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+    
+    public String getAllianceColor() {
+        return allianceColor;
+    }
+
+    public Auto getAuto() {
+        return auto;
+    }
+
+    public TeleOp getTeleop() {
+        return teleop;
+    }
+
+    public EndGame getEndgame() {
+        return endgame;
+    }
+    
+}
---
+++ ScoutApp/src/main/java/com/jacn/scoutapp/TeleOp.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jacn.scoutapp;
+
+/**
+ *
+ * @author bjtex
+ */
+public class TeleOp {
+    
+    private int teleOpScore;
+
+    private int teleOpUpperShotsMade;
+    private int teleOpLowerShotsMade;
+
+    private int teleOpBehavior;
+
+    private double intakePerform;
+    private double scoringPerform;
+    private double defensivePerform;
+
+    private int cargoBehaviour;
+
+    public TeleOp(int teleOpUpperShotsMade, int teleOpLowerShotsMade, int teleOpBehavior, double intakePerform, double scoringPerform, double defensivePerform, int cargoBehaviour) {
+        this.teleOpUpperShotsMade = teleOpUpperShotsMade;
+        this.teleOpLowerShotsMade = teleOpLowerShotsMade;
+        this.teleOpBehavior = teleOpBehavior;
+        this.intakePerform = intakePerform;
+        this.scoringPerform = scoringPerform;
+        this.defensivePerform = defensivePerform;
+        this.cargoBehaviour = cargoBehaviour;
+        
+        this.teleOpScore = teleOpUpperShotsMade * 2 + teleOpLowerShotsMade;
+    }
+
+    public int getScore() {
+        return teleOpScore;
+    }
+
+    public int getUpperShotsMade() {
+        return teleOpUpperShotsMade;
+    }
+
+    public int getLowerShotsMade() {
+        return teleOpLowerShotsMade;
+    }
+
+    public int getBehavior() {
+        return teleOpBehavior;
+    }
+
+    public double getIntakePerform() {
+        return intakePerform;
+    }
+
+    public double getScoringPerform() {
+        return scoringPerform;
+    }
+
+    public double getDefensivePerform() {
+        return defensivePerform;
+    }
+
+    public int getCargoBehaviour() {
+        return cargoBehaviour;
+    }
+    
+    
+}
