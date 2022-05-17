e65a29a8a155af5b85575691163bf46873dcf217
j a <bj.texasaldens@gmail.com>
2022-05-17 10:28:51 -0500

    graphics setup for one panel with text boxes and comboboxes fordata view choices

--- ScoutApp/src/scoutapp/GraphicsPanelScore.java
+++ ScoutApp/src/scoutapp/GraphicsPanelScore.java
@@ -74,5 +74,71 @@ public class GraphicsPanelScore extends JPanel {
         
         g.drawLine(yAxisWidth, xAxisHeight, yAxisWidth, borderHeight);        // Y axis
         g.drawLine(yAxisWidth, xAxisHeight, width - borderWidth, xAxisHeight);      // X Axis
+        
+             //graph rectangles
+//        int size = cases.keySet().size(); 
+//        int dateRangeSize = caseDays.get(caseDays.lastKey()) - caseDays.get(caseDays.firstKey());
+//        int rectangleWidth = useableWidth / dateRangeSize;
+//        int rectangleHeight = useableHeight / caseDays.keySet().size();
+//        
+//        int widthLoss = -1 * ((rectangleWidth * dateRangeSize) - useableWidth);
+//        int heightLoss = -1 * ((rectangleHeight * dateRangeSize) - useableHeight);
+//        
+//
+//        int currentDay = 0;
+//        int previousX = yAxisWidth + (widthLoss / 2);
+//        int previousY = (xAxisHeight - (heightLoss / 2)) - ( 7 * rectangleHeight);;
+//        for (int i=0; i < dateRangeSize; i++) {
+//            int count = 0;
+//            currentDay = caseDays.get(caseDays.firstKey()) + i;
+//            for (Integer key : caseDays.keySet()) {
+//                if (caseDays.get(key) <= currentDay) {
+//                    count++;
+//                }
+//            }
+//            
+//            int colorSteps = 255 / dateRangeSize;
+//            Color currentColor = new Color(colorSteps * i, 0, 0);
+//            g.setColor(currentColor);
+//            
+//            int x = yAxisWidth + (widthLoss / 2) + (i * rectangleWidth);
+//            int y = (xAxisHeight - (heightLoss / 2)) - ((count + 7) * rectangleHeight);
+//            int inWidth = rectangleWidth;
+//            int inHeight = rectangleHeight * count;
+//            g.fillRect(x, y, inWidth, inHeight);
+//            
+//            g.setColor(Color.BLUE);
+//            if (i % 3 == 0) {
+//                int inX1 = previousX;
+//                int inY1 = previousY;
+//                int inX2 = x;
+//                int inY2 = y + ((previousY - y) / 2);
+//                g.drawLine(inX1, inY1, inX2, inY2);
+//                g.drawLine(inX1, inY1 - 1, inX2, inY2 - 1);
+//                g.drawLine(inX1, inY1 - 2, inX2, inY2 - 2);
+//                g.drawLine(inX1, inY1 - 3, inX2, inY2 - 3);
+//                previousX = inX2;
+//                previousY = inY2;
+//            }
+//            
+//        }
+//        
+//         // add axis titles
+//        Color black = new Color(0, 0, 0);
+//        g.setColor(black);
+//         
+//        int xAxisTitleWidth = g.getFontMetrics().stringWidth("Total Cases Curve");
+//        int titleHeight = g.getFontMetrics().getHeight();
+//        
+//        //xAxis
+//        g.drawString("Total Cases Curve", ((useableWidth - xAxisTitleWidth) / 2) + yAxisWidth, xAxisHeight + ((borderHeight - titleHeight) / 2 ));
+//        
+//        String startDate = "" + (cases.get(cases.firstKey()).getReportDate().get(Calendar.MONTH) + 1) + "/" + cases.get(cases.firstKey()).getReportDate().get(Calendar.DAY_OF_MONTH) + "/"  + cases.get(cases.firstKey()).getReportDate().get(Calendar.YEAR);
+//        g.drawString(startDate, yAxisWidth - (g.getFontMetrics().stringWidth(startDate) / 2) + (widthLoss / 2), xAxisHeight + titleHeight);
+//        
+//        String endDate = "" + (cases.get(cases.lastKey()).getReportDate().get(Calendar.MONTH) + 1) + "/" + cases.get(cases.lastKey()).getReportDate().get(Calendar.DAY_OF_MONTH) + "/"  + cases.get(cases.lastKey()).getReportDate().get(Calendar.YEAR);
+//        g.drawString(endDate, yAxisWidth + useableWidth - (g.getFontMetrics().stringWidth(endDate) / 2) - (widthLoss / 2), xAxisHeight + titleHeight);
+//        
+//        g.drawString(Integer.toString(size), yAxisWidth - g.getFontMetrics().stringWidth(Integer.toString(size)) - 4, borderHeight + (titleHeight / 2));
     }
 }
